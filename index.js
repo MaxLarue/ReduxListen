825a900b2be70023e4cbf533852b964c8c278dd8
MaxLarue <maximilienlarue@gmail.com>
2018-02-12 10:10:46 +0100

    published

--- index.js
+++
@@ -1,4 +0,0 @@
-export {default as listenOnAction} from "./source/listenOnAction";
-export {actionListener} from "./source/listenOnAction";
-export {atPath, setAtPath} from "./source/translatePath";
-export {default as listenOnStateChange} from "./source/listenOnStateChange";
---
+++ source/index.js
@@ -0,0 +1,4 @@
+export {default as listenOnAction} from "./listenOnAction";
+export {actionListener} from "./listenOnAction";
+export {atPath, setAtPath} from "./translatePath";
+export {default as listenOnStateChange} from "./listenOnStateChange";
